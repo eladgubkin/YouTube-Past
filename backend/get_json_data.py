f8e8aec79e2f8236db00e5064657286afebe9535
Elad Gubkin <Eladox3@gmail.com>
2021-09-21 11:41:12 +0300

    + search-history.json

--- backend/get_json_data.py
+++ backend/get_json_data.py
@@ -1,10 +1,10 @@
 import json
 
-file_name = './watch-history.json'
+def get_json_data(history):
 
+    filename = f'./{history}.json'
 
-def get_json_data():
-    with open(file_name, encoding='utf-8') as json_data:
+    with open(filename, encoding='utf-8') as json_data:
         data = json.load(json_data)
 
     return data
