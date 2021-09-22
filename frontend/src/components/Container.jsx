fcac5762f9b742bf3ca602bd896638861d05f5ad
Elad Gubkin <Eladox3@gmail.com>
2021-09-22 21:19:19 +0300

    paragraph

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Graph from "./Graph";
 
 const Container = ({ data }) => {
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }} id="Container">
       <Toolbar />
-      <Graph data={data} />
+      {/* <Graph data={data} /> */}
+      <Typography paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+        imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+        velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate eu
+        scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
+        lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+        ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+      </Typography>
       <code>{JSON.stringify(data, null, 2)}</code>
     </Box>
   );
