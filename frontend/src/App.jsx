6df0326dfa77b9270f6934f8ebb3752b8a55fbba
Elad Gubkin <Eladox3@gmail.com>
2021-09-22 13:29:52 +0300

    on hold for the moment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import Layout from "./components/Layout";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [channelName, setChannelName] = useState("");
+  const [channelName, setChannelName] = useState("Coin Bureau");
   const [time, setTime] = useState({ year: null, month: null, day: null });
 
   const getRandomVideo = () => {
@@ -47,21 +48,27 @@ const App = () => {
     fetch("http://127.0.0.1:5000/views")
       .then((result) => result.json())
       .then((data) => {
-        setData(data);
+        setData(
+          data.map((video) => ({
+            link: video[0],
+            viewTimes: video[1],
+          }))
+        );
       });
   };
 
   return (
-    <div>
-      <h1>App</h1>
-      <button onClick={getRandomVideo}>Get a random video</button>
+    <>
+      <Layout />
+
+      {/* <button onClick={getRandomVideo}>Get a random video</button>
       <button onClick={getByChannel}>Get all videos by channel name:</button>
       <input type="text" onChange={setChannelNameCB} value={channelName} />
       <input type="date" min="2019-04-08" max="2021-09-20" onChange={setDateTimeFunc} />
       <button onClick={getByTime}>Get by date</button>
       <button onClick={getByMostViews}>Get the most viewed Video by you</button>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+      <pre>{JSON.stringify(data, null, 2)}</pre> */}
+    </>
   );
 };
 
