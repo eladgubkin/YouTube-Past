922c3c69ea54984e544a326e15df739bca392200
Elad Gubkin <Eladox3@gmail.com>
2021-09-24 01:02:11 +0300

    Update get_json_data.py

--- backend/get_json_data.py
+++ backend/get_json_data.py
@@ -2,10 +2,9 @@ import json
 
 def get_json_data(history):
 
-    filename = f'./{history}.json'
+    filename = f'../{history}.json'
 
     with open(filename, encoding='utf-8') as json_data:
         data = json.load(json_data)
 
     return data
-
