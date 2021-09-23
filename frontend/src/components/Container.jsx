ae56cc9595fc083a69e91567392a35b55121f3cf
Elad Gubkin <Eladox3@gmail.com>
2021-09-24 01:01:14 +0300

    Added Grid

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -3,13 +3,28 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Graph from "./Graph";
+import { Grid } from "@mui/material";
 
 const Container = ({ data }) => {
   return (
-    <Box component="main" sx={{ flexGrow: 1, p: 3 }} id="Container">
+    <Box component="main" sx={{ flexGrow: 1 }}>
       <Toolbar />
-      {/* <Graph data={data} /> */}
-      <code>{JSON.stringify(data, null, 2)}</code>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12}>
+          {/* <Graph data={data} /> */}
+          <h1>Lol</h1>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <h1>Lol</h1>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <h1>Lol</h1>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
