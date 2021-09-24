a72c477c208df4839898609459cfc8cd38051de0
Elad Gubkin <Eladox3@gmail.com>
2021-09-24 21:06:40 +0300

    deleted

--- backend/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-routes = Blueprint('routes', __name__)
-
-from .get_random_vid import *
-from .get_by_channel import * 
-from .get_by_time import *
-from .get_by_most_views import *
