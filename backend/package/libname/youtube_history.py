66909b31029e08433fcf13e5254fec8fb80acfdc
Elad Gubkin <Eladox3@gmail.com>
2021-09-24 18:33:15 +0300

    YoutubeHistory class, inherit from WatchHistory and searchHistory

--- backend/package/libname/youtube_history.py
+++ backend/package/libname/youtube_history.py
@@ -1,69 +1,46 @@
-import sys
 import pandas as pd
-import numpy as np
+import sys
+from watch_history import WatchHistory
+from search_history import SearchHistory
 
-class Data:
+
+class YoutubeHistory(WatchHistory, SearchHistory):
     def __init__(self):
-        self.df = []
+        self.watch_df = []
+        self.search_df = []
+        WatchHistory.__init__(self, self.watch_df)
+        SearchHistory.__init__(self, self.search_df)
+        self.merged = {}
 
-    def read_data(self):
+    def read_file(self):
         try:
-            filename = sys.argv[1]
-        except IndexError:
-            print('filename must be an argument')
+            watch_history_file, search_history_file = sys.argv[1:]
+        except IndexError and ValueError:
+            print("first argument: 'watch-history.json', second argument: 'search-history.json'")
+            quit()
 
-        # read json as records list
-        self.df = pd.read_json(filename, orient='records')
+        try:
+            self.watch_df = pd.read_json(watch_history_file, orient='records')
+            self.search_df = pd.read_json(search_history_file, orient='records')
+        except ValueError:
+            print('Wrong files')
+            quit()
 
-        return self.df
+        return
 
 
-    def parse_data(self):
-        # exclude 'Visited YouTube Music' title
-        self.df = self.df.drop(self.df[self.df['title'] == 'Visited YouTube Music'].index)
+    def merge_dfs(self):
+        self.merged = {
+            'watch_history': self.watch_df.to_dict(orient='records'),
+            'search_history': self.search_df.to_dict(orient='records')
+        }
 
-        # replace NaN values with zeros
-        self.df = self.df.replace(np.nan, 0)
-
-        # if no 'subtitles' add subtitles of None but still iterable - [{"name": None, "url": None}]
-        self.df['subtitles'] = [[{"name": None, "url": None}] if subtitle == 0 else subtitle for subtitle in self.df.subtitles]
-
-        # destruct 'subtitles' to "channelName" and "channelUrl"
-        self.df['channelName'], self.df['channelUrl'] = zip(*self.df['subtitles'].map(lambda channel: (channel[0]['name'], channel[0]['url'])))
-
-        # exclude YouTube Ads, products and subtitles (after destruct subtitles)
-        self.df = self.df.drop(columns=['products', 'details', 'subtitles'], errors='ignore')
-
-        # remove "watched" from every beginning of title
-        self.df['title'] = self.df['title'].str[8:]
-
-        # count the frequency that 'titleUrl' and 'channelUrl' occurs
-        self.df['timesViewedVideo'] = self.df.groupby(['titleUrl'])['titleUrl'].transform('count')
-        self.df['timesViewedChannel'] = self.df.groupby(['channelUrl'])['channelUrl'].transform('count')
-
-        # Sort by timesViewedVideo
-        self.df = self.df.sort_values(by=['timesViewedVideo'], ascending=True)
-
-        # get Video ID from 'titleUrl'
-        self.df['videoId'] = self.df['titleUrl'].str[-11:]
-
-        # get Channel ID from 'channelUrl'
-        self.df['channelId'] = self.df['channelUrl'].str[-24:]
-
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            print(list(self.df))
+        return self.merged
 
 
     def get_data(self):
-        # serve json as records list
-        self.df = self.df.to_json(orient='records')
-        return self.df
-
-
-
-data = Data()
-
-data.read_data()
-data.parse_data()
-
+        self.read_file()
+        self.parse_watch_history_data()
+        self.parse_search_history_data()
+        return self.merge_dfs()
 
