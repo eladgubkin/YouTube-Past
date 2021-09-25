c36ce66869a83aaa3e7629a994bdc4e1e26ec0a5
Elad Gubkin <Eladox3@gmail.com>
2021-09-25 23:28:42 +0300

    fetch data on mount

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,45 @@
 import React, { useState, useEffect } from "react";
-import Layout from "./components/Layout";
+import Box from "@mui/material/Box";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
+import { CssBaseline } from "@mui/material";
 
 const App = () => {
   const [data, setData] = useState({});
+  const [drawerWidth] = useState(300);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
-  // useEffect(() => {
-  //   fetch("http://127.0.0.1:5000/", { method: "POST" })
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       setData({
-  //         watchHistory: JSON.parse(res.watchHistory),
-  //         searchHistory: JSON.parse(res.searchHistory),
-  //       });
-  //     });
-  // }, []);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-  console.log(data);
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/", { method: "POST" })
+      .then((res) => res.json())
+      .then((res) => {
+        setData({
+          watchHistory: JSON.parse(res.watchHistory),
+          searchHistory: JSON.parse(res.searchHistory),
+        });
+        // localStorage.setItem("data", data);
+      });
+  }, []);
 
-  return (
-    <>
-      <Layout />
-    </>
-  );
+  // console.log(data);
+
+  if (Object.keys(data).length === 0) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <Box sx={{ display: "flex", height: "100%" }}>
+        <CssBaseline />
+        <Header drawerWidth={drawerWidth} handleDrawerToggle={handleDrawerToggle} />
+        <Sidebar drawerWidth={drawerWidth} handleDrawerToggle={handleDrawerToggle} mobileOpen={mobileOpen} />
+        <Main mobileOpen={mobileOpen} data={data["searchHistory"]} />
+      </Box>
+    );
+  }
 };
 
 export default App;
