09e7a0415f9c93954cca7379d2d10c4a33c1cab7
Elad Gubkin <Eladox3@gmail.com>
2021-09-25 23:29:46 +0300

    Added base UI for sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,18 +3,87 @@ import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import ytLogo from "../yt-logo.png";
+import ytMusicLogo from "../yt-music-logo.png";
+import List from "@mui/material/List";
+import { ListItem } from "@mui/material";
 
 const Sidebar = ({ drawerWidth, handleDrawerToggle, mobileOpen }) => {
   const drawer = (
-    <div>
-      <Toolbar>
-        <Typography variant="h6" noWrap component="div">
-          YouTube History
-        </Typography>
+    <aside style={{ height: "100%", fontSize: "0.9em" }}>
+      <Toolbar style={{ display: "flex", justifyContent: "space-between", margin: "10px 30px " }}>
+        <img src={ytLogo} alt="yt-logo.png" style={{ width: 60 }} />
+        <img src={ytMusicLogo} alt="yt-logo.png" style={{ width: 50, opacity: 0.2 }} />
       </Toolbar>
       <Divider />
-    </div>
+      <List
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ListItem style={{ justifyContent: "center", cursor: "pointer" }}>All Time</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>2018</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>2019</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>2020</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>2021</ListItem>
+      </List>
+      <List style={{ display: "grid", gridTemplateColumns: "repeat(5, 1fr)" }}>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Jan</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Feb</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Mar</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Apr</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>May</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Jun</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Jul</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Aug</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Sep</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Oct</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Nov</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Dec</ListItem>
+      </List>
+      <List style={{ display: "grid", gridTemplateColumns: "repeat(5, 1fr)" }}>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>1</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>2</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>3</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>4</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>5</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>6</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>7</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>8</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>9</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>10</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>11</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>12</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>13</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>14</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>15</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>16</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>17</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>18</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>19</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>20</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>21</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>22</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>23</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>24</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>25</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>26</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>27</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>28</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>29</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>30</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>31</ListItem>
+      </List>
+      <List style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Morning</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Afternoon</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Evening</ListItem>
+        <ListItem style={{ justifyContent: "center", opacity: 0.2, cursor: "pointer" }}>Night</ListItem>
+      </List>
+    </aside>
   );
 
   return (
