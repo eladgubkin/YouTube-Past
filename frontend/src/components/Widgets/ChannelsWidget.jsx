dbb459ade848321086ad92a4a40697ea9be8a0c0
Elad Gubkin <Eladox3@gmail.com>
2021-09-26 23:21:25 +0300

    Bar chart working

--- frontend/src/components/Widgets/ChannelsWidget.jsx
+++ frontend/src/components/Widgets/ChannelsWidget.jsx
@@ -1,12 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import highcharts3d from "highcharts/highcharts-3d";
 
-const ChannelsWidget = () => {
-  return (
-    <>
-      <h1>Channels Widget</h1>
-      <h2>Bar Chart</h2>
-    </>
-  );
+highcharts3d(Highcharts);
+
+const ChannelsWidget = ({ data }) => {
+  const [newData, setNewData] = useState(data);
+
+  useEffect(() => {
+    let array = [];
+
+    newData.sort((a, b) =>
+      a.timesViewedChannel > b.timesViewedChannel ? 1 : b.timesViewedChannel > a.timesViewedChannel ? -1 : 0
+    );
+
+    newData.forEach((videoObj) => {
+      array.push([videoObj.channelName, videoObj.timesViewedChannel]);
+    });
+
+    setNewData(
+      Array.from(new Set(array.map(JSON.stringify)), JSON.parse)
+        .slice(1)
+        .slice(-15)
+    );
+  }, []);
+
+  const options = {
+    chart: {
+      type: "column",
+      backgroundColor: "#0f0c29",
+      options3d: {
+        enabled: true,
+        alpha: 0,
+        beta: 0,
+        depth: 8,
+        viewDistance: 75,
+      },
+    },
+    title: {
+      text: "Top YouTube Channels",
+      style: {
+        color: "#fff",
+        fontWeight: "bold",
+      },
+    },
+    plotOptions: {
+      column: {
+        depth: 25,
+      },
+      series: {
+        colorByPoint: true,
+        colors: [
+          "#012a4a",
+          "#013a63",
+          "#01497c",
+          "#014f86",
+          "#2a6f97",
+          "#2c7da0",
+          "#468faf",
+          "#61a5c2",
+          "#89c2d9",
+          "#a9d6e5",
+        ],
+      },
+    },
+
+    legend: {
+      enabled: false,
+    },
+
+    credits: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        data: newData,
+      },
+    ],
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
 };
 
 export default ChannelsWidget;
--- frontend/src/components/Widgets/VideosWidget.jsx
+++ frontend/src/components/Widgets/VideosWidget.jsx
@@ -1,12 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import highcharts3d from "highcharts/highcharts-3d";
 
-const VideosWidget = () => {
-  return (
-    <>
-      <h1>Videos Widget</h1>
-      <h2>Pie Chart</h2>
-    </>
-  );
+highcharts3d(Highcharts);
+
+const VideosWidget = ({ data }) => {
+  const [newData, setNewData] = useState(data);
+
+  useEffect(() => {
+    let array = [];
+
+    newData.sort((a, b) =>
+      a.timesViewedVideo > b.timesViewedVideo ? 1 : b.timesViewedVideo > a.timesViewedVideo ? -1 : 0
+    );
+
+    newData.forEach((videoObj) => {
+      array.push([videoObj.title, videoObj.timesViewedVideo]);
+    });
+
+    setNewData(
+      Array.from(new Set(array.map(JSON.stringify)), JSON.parse)
+        .slice(1)
+        .slice(-15)
+    );
+  }, []);
+
+  const options = {
+    chart: {
+      type: "column",
+      backgroundColor: "#0f0c29",
+      options3d: {
+        enabled: true,
+        alpha: 0,
+        beta: 0,
+        depth: 8,
+        viewDistance: 75,
+      },
+    },
+    title: {
+      text: "Top YouTube Videos",
+      style: {
+        color: "#fff",
+        fontWeight: "bold",
+      },
+    },
+    plotOptions: {
+      column: {
+        depth: 25,
+      },
+    },
+
+    legend: {
+      enabled: false,
+    },
+
+    credits: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        data: newData,
+      },
+    ],
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
 };
 
 export default VideosWidget;
