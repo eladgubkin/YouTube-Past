579dda069f47d72c79a0a6a7a6152b8f0e90e0e3
Elad Gubkin <Eladox3@gmail.com>
2021-09-26 23:21:47 +0300

    Update StatisticsWidget.jsx

--- frontend/src/components/Widgets/StatisticsWidget.jsx
+++ frontend/src/components/Widgets/StatisticsWidget.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const StatisticsWidget = () => {
+const StatisticsWidget = ({ data }) => {
   return (
     <>
       <h1>Statistics Widget</h1>
+      <h3>Total Videos: {data["watchHistory"].length}</h3>
+      <h3>Total Searches: {data["searchHistory"].length}</h3>
     </>
   );
 };
