f739bb4c995330cb9cb79e54ed732bdcc8a13d38
Elad Gubkin <Eladox3@gmail.com>
2021-09-26 23:22:06 +0300

    Update TableWidget.jsx

--- frontend/src/components/Widgets/TableWidget.jsx
+++ frontend/src/components/Widgets/TableWidget.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+// import { DataGrid } from "@mui/x-data-grid";
 
-const TableWidget = () => {
+const TableWidget = ({ data }) => {
   return (
     <>
-      <h1>Table Widget</h1>
+      {/* <div style={{ height: 400, width: "100%" }}>
+        <div style={{ display: "flex", height: "100%" }}>
+          <div style={{ flexGrow: 1 }}>
+            <DataGrid
+              columns={[{ field: "video" }, { field: "channel" }, { field: "views" }, { field: "date" }]}
+              rows={data.map((video) => {
+                return {
+                  video: video.title,
+                  channel: video.channelName,
+                  views: video.timesViewedVideo,
+                  date: video.time,
+                };
+              })}
+            />
+          </div>
+        </div>
+      </div> */}
+
+      <h1>Table</h1>
     </>
   );
 };
