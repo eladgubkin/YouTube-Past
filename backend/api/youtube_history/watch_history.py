a1931e8db41dd3b091f9174b4c804f5942efaac5
Elad Gubkin <Eladox3@gmail.com>
2021-09-27 23:07:39 +0300

    Remove titleUrl == 0

--- backend/api/youtube_history/watch_history.py
+++ backend/api/youtube_history/watch_history.py
@@ -15,6 +15,7 @@ class WatchHistory:
 
         # if no 'subtitles' add subtitles of None but still iterable (no error raisen) - [{"name": None, "url": None}]
         self.watch_df['subtitles'] = [[{"name": None, "url": None}] if subtitle == 0 else subtitle for subtitle in self.watch_df.subtitles]
+ 
 
         # destruct 'subtitles' to "channelName" and "channelUrl"
         self.watch_df['channelName'], self.watch_df['channelUrl'] = zip(*self.watch_df['subtitles'].map(lambda channel: (channel[0]['name'], channel[0]['url'])))
@@ -29,8 +30,8 @@ class WatchHistory:
         self.watch_df['timesViewedVideo'] = self.watch_df.groupby(['titleUrl'])['titleUrl'].transform('count')
         self.watch_df['timesViewedChannel'] = self.watch_df.groupby(['channelUrl'])['channelUrl'].transform('count')
 
-        # Sort by timesViewedVideo
-        # self.watch_df = self.watch_df.sort_values(by=['timesViewedChannel'], ascending=False)
+        # Remove all rows with titleUrl == 0
+        self.watch_df = self.watch_df[self.watch_df.titleUrl != 0]
 
         # get Video ID from 'titleUrl'
         self.watch_df['videoId'] = self.watch_df['titleUrl'].str[-11:]
