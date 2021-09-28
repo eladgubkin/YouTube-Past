5e5308677c669f5dd9c7b5fa22bbab25955e5fe9
Elad Gubkin <Eladox3@gmail.com>
2021-09-28 23:58:07 +0300

    Removed comments

--- frontend/src/components/Widgets/WordCloudWidget.jsx
+++ frontend/src/components/Widgets/WordCloudWidget.jsx
@@ -34,42 +34,11 @@ const WordCloudWidget = ({ data }) => {
 
   wordCloudData = wordCloudData.sort((a, b) => (a.weight > b.weight ? 1 : b.weight > a.weight ? -1 : 0));
 
-  // const makeScale = (domain, range) => {
-  //   const minDomain = domain[0];
-  //   const maxDomain = domain[1];
-  //   const rangeStart = range[0];
-  //   const rangeEnd = range[1];
-
-  //   return (value) => {
-  //     return rangeStart + (rangeEnd - rangeStart) * ((value - minDomain) / (maxDomain - minDomain));
-  //   };
-  // };
-
-  // const minWeight = wordCloudData.reduce(
-  //   (min, word) => (word.weight < min ? word.weight : min),
-  //   wordCloudData[0].weight
-  // );
-  // const maxWeight = wordCloudData.reduce(
-  //   (max, word) => (word.weight > max ? word.weight : max),
-  //   wordCloudData[0].weight
-  // );
-  // const scale = makeScale([minWeight, maxWeight], [0.3, 1]);
-
-  // const scaledData = wordCloudData.map((word) => ({
-  //   name: word.name,
-  //   weight: word.weight,
-  //   opacity: `${scale(word.weight)}`,
-  // }));
-
   const options = {
     series: [
       {
         type: "wordcloud",
         data: wordCloudData.splice(-150, Math.ceil(wordCloudData.length)),
-        // rotation: {
-        //   from: 0,
-        //   to: 0,
-        // },
         minFontSize: 7,
         style: {
           fontFamily: "Arial",
@@ -104,7 +73,6 @@ const WordCloudWidget = ({ data }) => {
       followPointer: true,
       style: {
         color: "#fff",
-        textTransform: "uppercase",
       },
     },
   };
