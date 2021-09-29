b7f5a9e03e0d0019578c103478f6a2fbfdc05cd9
Elad Gubkin <Eladox3@gmail.com>
2021-09-29 20:45:03 +0300

    Removed unused import

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Typography from "@mui/material/Typography";
 
 const Header = ({ drawerWidth, handleDrawerToggle }) => {
   return (
