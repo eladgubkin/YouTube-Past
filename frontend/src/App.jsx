0e0cd0b25f41799ded1bc89a0956f14523c56b62
Elad Gubkin <Eladox3@gmail.com>
2021-10-01 23:39:46 +0300

    Removed Mui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
-import { CssBaseline } from "@mui/material";
 
 const App = () => {
   const [data, setData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [drawerWidth] = useState(300);
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/", { method: "POST" })
@@ -29,14 +18,7 @@ const App = () => {
   if (Object.keys(data).length === 0) {
     return <h1>Loading...</h1>;
   } else {
-    return (
-      <Box sx={{ display: "flex", height: "100%" }}>
-        {/* <CssBaseline /> */}
-        <Header drawerWidth={drawerWidth} handleDrawerToggle={handleDrawerToggle} />
-        <Sidebar drawerWidth={drawerWidth} handleDrawerToggle={handleDrawerToggle} mobileOpen={mobileOpen} />
-        <Main mobileOpen={mobileOpen} data={data} />
-      </Box>
-    );
+    return <Main data={data} />;
   }
 };
 
