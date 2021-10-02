7ffd9dfd16d8001bf01300ecc9042f809723f005
Elad Gubkin <Eladox3@gmail.com>
2021-10-03 02:19:56 +0300

    Removed old styles

--- frontend/src/components/styles/Header.styled.js
+++ frontend/src/components/styles/Header.styled.js
@@ -1,10 +1,5 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  position: sticky;
-  z-index: 1;
-  top: 0;
   width: 100%;
-  height: 75px;
-  background: #011627;
 `;
