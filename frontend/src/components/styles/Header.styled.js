6b9b776b1ab01311da5199d5c9ec40125164065e
Elad Gubkin <Eladox3@gmail.com>
2021-10-02 23:09:31 +0300

    Styled

---
+++ frontend/src/components/styles/Header.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  z-index: 1;
+  top: 0;
+  width: 100%;
+  height: 75px;
+  background: #011627;
+`;
