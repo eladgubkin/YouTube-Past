595db786e3861c6933f945689a4e2a0883bb0bb7
Elad Gubkin <Eladox3@gmail.com>
2021-10-02 23:08:53 +0300

    Clear Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,31 +1,11 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+import { StyledHeader } from "./styles/Header.styled";
 
-const Header = ({ drawerWidth, handleDrawerToggle }) => {
+const Header = () => {
   return (
-    <AppBar
-      position="fixed"
-      sx={{
-        width: { lg: `calc(100% - ${drawerWidth}px)` },
-        ml: { lg: `${drawerWidth}px` },
-        background: "#1B2845",
-      }}
-    >
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { lg: "none" } }}
-        >
-          <MenuIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <StyledHeader>
+      <span>Button</span>
+    </StyledHeader>
   );
 };
 
