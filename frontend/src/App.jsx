59f7cfdabd9867008bb95015876cb2617c741ede
Elad Gubkin <Eladox3@gmail.com>
2021-10-03 02:19:24 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "./components/Layout";
-import "react-pro-sidebar/dist/css/styles.css";
-// import "./components/styles/App.scss";
 
 const App = () => {
-  return <Layout />;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 };
 
 export default App;
