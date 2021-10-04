9f51fee90ea3eaf706efb4118ded9c95a48c52ce
Elad Gubkin <Eladox3@gmail.com>
2021-10-04 23:42:34 +0300

    Timeline

--- frontend/src/components/Widgets/TimelineWidget.jsx
+++ frontend/src/components/Widgets/TimelineWidget.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const TimelineWidget = () => {
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <div style={{ width: "100%", height: "100%", background: "#1B2845" }}>hello</div>
+      <div style={{ width: "100%", height: "100%", background: "#1B2845" }}>
+        <h1>Timeline</h1>
+      </div>
     </div>
   );
 };
