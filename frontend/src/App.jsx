8e8c1321c1725f904efd4e0969b4948f5a088a59
Elad Gubkin <Eladox3@gmail.com>
2021-10-04 23:40:42 +0300

    Added Context API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
-import Layout from "./components/Layout";
+import React, { useState, useEffect } from "react";
+import { DataContext } from "./components/contexts/data";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/", { method: "POST" })
+      .then((res) => res.json())
+      .then((res) => {
+        setData({
+          watchHistory: JSON.parse(res.watchHistory),
+          searchHistory: JSON.parse(res.searchHistory),
+        });
+      });
+  }, []);
+
   return (
-    <>
-      <Layout />
-    </>
+    <DataContext.Provider value={data}>
+      {data ? <Layout /> : <h1>Loading...</h1>}
+    </DataContext.Provider>
   );
 };
 
