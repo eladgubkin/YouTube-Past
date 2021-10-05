9fba8799b2d0568aec2e672c232d6e4871fbb52f
Elad Gubkin <Eladox3@gmail.com>
2021-10-05 23:28:44 +0300

    Removed outer divs

--- frontend/src/components/Widgets/TimelineWidget.jsx
+++ frontend/src/components/Widgets/TimelineWidget.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const TimelineWidget = () => {
-  return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <div style={{ width: "100%", height: "100%", background: "#1B2845" }}>
-        <h1>Timeline</h1>
-      </div>
-    </div>
-  );
+  return <h1>Timeline</h1>;
 };
 
 export default TimelineWidget;
