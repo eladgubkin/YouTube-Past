d969ea9ccab10904e300c7c1a51046e297528c08
Elad Gubkin <Eladox3@gmail.com>
2021-10-09 23:40:08 +0300

    Test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { DataContext } from "./components/contexts/data";
-import Layout from "./components/Layout/Layout";
+// import { DataContext } from "./components/contexts/data";
+// import Layout from "./components/Layout/Layout";
+import Test from "./components/Test";
 
 const App = () => {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/", { method: "POST" })
-      .then((res) => res.json())
-      .then((res) => {
-        setData({
-          watchHistory: JSON.parse(res.watchHistory),
-          searchHistory: JSON.parse(res.searchHistory),
-        });
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/", { method: "POST" })
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setData({
+  //         watchHistory: JSON.parse(res.watchHistory),
+  //         searchHistory: JSON.parse(res.searchHistory),
+  //       });
+  //     });
+  // }, []);
 
   return (
-    <DataContext.Provider value={data}>
-      {data ? <Layout /> : <h1>Loading...</h1>}
-    </DataContext.Provider>
+    // <DataContext.Provider value={data}>
+    // {data ? <Layout /> : <h1>Loading...</h1>}
+    <Test />
+    // </DataContext.Provider>
   );
 };
 
