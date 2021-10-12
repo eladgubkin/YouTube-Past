b758c1f40e5c2bc00d360780593557e4b38f5ce2
Elad Gubkin <Eladox3@gmail.com>
2021-10-12 23:51:09 +0300

    Schema at app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_graphql import GraphQLView
-from schema import schema
+from graphene import Schema
+from mutations import MyMutations, MyQueries
+
 
 app = Flask(__name__)
 CORS(app)
+
+schema = Schema(query=MyQueries, mutation=MyMutations)
 
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
     'graphql',
