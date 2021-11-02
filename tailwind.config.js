c729c4409ebdfb842b62104418d189ae002ac2cf
Elad Gubkin <Eladox3@gmail.com>
2021-11-02 22:53:10 +0200

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  darkMode: false,
+};
