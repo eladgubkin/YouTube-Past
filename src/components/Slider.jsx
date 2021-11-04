ca5bd336841b5a50922aed43aefa353b52ca9de2
Elad Gubkin <Eladox3@gmail.com>
2021-11-04 23:49:15 +0200

    D3 example with circles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { select } from "d3";
 
 export const Slider = () => {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", (value) => value)
+      .attr("cx", (value) => value * 2)
+      .attr("cy", (value) => value * 2)
+      .attr("stroke", "red");
+  }, [data]);
+
   return (
-    <div>
-      <h1>DateTime Slider</h1>
-    </div>
+    <>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Update data
+      </button>{" "}
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
+        Filter data
+      </button>
+    </>
   );
 };
