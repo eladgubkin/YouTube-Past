7e1037435db02e3c5131a98681e159cf3100bfbd
Elad Gubkin <Eladox3@gmail.com>
2021-11-05 22:26:56 +0200

    Added FilesContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import { DataContext } from "./Contexts/DataContext";
-import "tailwindcss/tailwind.css";
-import _ from "lodash";
-
-// Views
+import { FilesContext } from "./Contexts/FilesContext";
+import { FilesContextProvider } from "./Contexts/FilesContext";
 import { Landing } from "./views/Landing";
-import { Import } from "./views/Import";
+import { MainApp } from "./views/MainApp";
 
 export const App = () => {
-  const [watchHistoryData, setWatchHistoryData] = useState([]);
-  const [searchHistoryData, setSearchHistoryData] = useState([]);
-
   return (
-    <DataContext.Provider
-      value={{
-        watchHistoryData,
-        setWatchHistoryData,
-        searchHistoryData,
-        setSearchHistoryData,
-      }}
-    >
-      <BrowserRouter>
-        <Route path="/" component={Landing} />
-        <Route path="/import" component={Import} />
-      </BrowserRouter>
-    </DataContext.Provider>
+    <FilesContextProvider>
+      <FilesContext.Consumer>
+        {(ctx) =>
+          !ctx.watchHistoryData?.length || !ctx.searchHistoryData?.length ? (
+            <Landing />
+          ) : (
+            <MainApp />
+          )
+        }
+      </FilesContext.Consumer>
+    </FilesContextProvider>
   );
 };
