60f7ddb7213c8b900b5c48fdf8897391898f1ff6
Elad Gubkin <Eladox3@gmail.com>
2021-11-08 23:43:52 +0200

    Implement parsing for year and month

--- src/utils/charts/weekBars/parseWeekBarsData.js
+++ src/utils/charts/weekBars/parseWeekBarsData.js
@@ -1,19 +1,30 @@
 import _ from "lodash";
 
-export const parseWeekBarsData = (watchHistoryData) => {
-  const data = _.map(
-    _.countBy(
-      watchHistoryData.map((video) => ({
-        videoId: video.videoId,
-        day: video.time.day(),
-      })),
-      "day"
-    ),
-    (val, key) => ({
-      day: key,
-      count: val,
-    })
-  );
+export const parseWeekBarsData = (
+  watchHistoryData,
+  year = 2020,
+  month = 10
+) => {
+  let data = [];
+
+  data = watchHistoryData.map((video) => ({
+    year: video.time.year(),
+    month: video.time.month(),
+    day: video.time.day(),
+    videoId: video.videoId,
+  }));
+
+  if (year) data = _.remove(data, { year });
+  console.log(data);
+
+  if (month) data = _.remove(data, { month });
+  console.log(data);
+
+  // Count
+  data = _.map(_.countBy(data, "day"), (count, day) => ({
+    day,
+    count,
+  }));
 
   return data;
 };
