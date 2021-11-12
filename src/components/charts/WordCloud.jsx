345bf5e90a774ed57a9aefdb2211e2c060e5ea0c
Elad Gubkin <Eladox3@gmail.com>
2021-11-12 22:58:37 +0200

    D3-ish

--- src/components/charts/WordCloud.jsx
+++ src/components/charts/WordCloud.jsx
@@ -1,14 +1,62 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { ChartsContext } from "../../contexts/ChartsContext";
 import { FilesContext } from "../../contexts/FilesContext";
 import { parseWordCloudData } from "../../utils/charts/wordCloud/parseWordCloudData";
+import { select, cloud } from "d3";
+
+const myWords = [
+  "Hello",
+  "Everybody",
+  "How",
+  "Are",
+  "You",
+  "Today",
+  "It",
+  "Is",
+  "A",
+  "Lovely",
+  "Day",
+  "I",
+  "Love",
+  "Coding",
+  "In",
+  "My",
+  "Van",
+  "Mate",
+];
+
+const margin = { top: 10, right: 10, bottom: 10, left: 10 };
+const width = 450 - margin.left - margin.right;
+const height = 450 - margin.top - margin.bottom;
 
 export const WordCloud = () => {
   const { wordCloudData, setWordCloudData } = useContext(ChartsContext);
   const { searchHistoryData } = useContext(FilesContext);
+  const svgRef = useRef();
 
   useEffect(() => {
     setWordCloudData(parseWordCloudData(searchHistoryData));
+
+    const svg = select(svgRef.current);
+    svg
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    const layout = layout
+      .cloud()
+      .size([width, height])
+      .words(
+        myWords.map((d) => {
+          return { text: d };
+        })
+      )
+      .padding(10)
+      .fontSize(60)
+      .on("end", draw);
+
+    layout.start();
   }, []);
 
   return (
@@ -19,6 +67,7 @@ export const WordCloud = () => {
       >
         WordCloud
       </button>
+      <svg ref={svgRef} />
     </>
   );
 };
