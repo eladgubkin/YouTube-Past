6db5d1c73c0661ddaeff9c8339a1231ad6f75bdf
Elad Gubkin <Eladox3@gmail.com>
2021-11-20 23:50:53 +0200

    Data parsing for year and month. TODO: for date

--- src/utils/charts/dayBars/parseDayBarsData.js
+++ src/utils/charts/dayBars/parseDayBarsData.js
@@ -1,24 +1,54 @@
 import _ from "lodash";
 
-export const parseDayBarsData = (
-  watchHistoryData,
-  filterByYear = 2020,
-  filterByMonth = undefined
-) => {
+export const parseDayBarsData = (watchHistoryData) => {
   let data = [];
 
   data = watchHistoryData.map((video) => ({
     year: video.time.year(),
     month: video.time.month(),
     day: video.time.day(),
+    date: video.time.date(),
     videoId: video.videoId,
   }));
 
-  if (filterByYear) data = _.remove(data, { year: filterByYear });
-  if (filterByMonth) data = _.remove(data, { month: filterByMonth });
+  let dataByYear = {};
+  let dataByMonth = {};
+  let dataByDates = {};
 
-  // Count
-  data = _.map(_.countBy(data, "day"), (count, day) => Math.round(count / 48), 2);
+  const years = _.uniqBy(_.map(data, (dta) => dta.year));
+  const months = _.uniqBy(_.map(data, (dta) => dta.month));
+  const days = _.uniqBy(_.map(data, (dta) => dta.day));
+  const dates = _.uniqBy(_.map(data, (dta) => dta.date));
 
-  return data;
+  // Filter by years
+  _.forEach(years, (year) => {
+    dataByYear[year] = _.filter(data, (o) => o.year === year);
+  });
+
+  // Filter by months
+  _.mapValues(dataByYear, (dta, year) => {
+    months.map((month) => {
+      dataByMonth[year] = {
+        ...dataByMonth[year],
+        [month]: _.filter(dta, (o) => o.month === month),
+      };
+    });
+  });
+
+  // Filter by dates
+  // _.mapValues(dataByMonth, (dta, year) => {
+  //   _.mapValues(dta, (d, month) => {
+  //     dates.map((date) => {
+  //       dataByDates[year] = {
+  //         ...dataByDates[year],
+  //         [month]: {
+  //           ...dataByDates[year[month]],
+  //           [date]: _.filter(d, (o) => o.date === date),
+  //         },
+  //       };
+  //     });
+  //   });
+  // });
+
+  return dataByMonth;
 };
