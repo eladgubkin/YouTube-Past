0756ea6b70378c5bde979ee5f947f08874d36e21
Elad Gubkin <Eladox3@gmail.com>
2021-11-20 23:49:03 +0200

    Removed unnecessary import

--- src/components/charts/WordCloud.jsx
+++ src/components/charts/WordCloud.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { ChartsContext } from "../../contexts/ChartsContext";
 
 export const WordCloud = () => {
