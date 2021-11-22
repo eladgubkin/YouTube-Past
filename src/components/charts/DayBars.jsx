6655193fa78cd511dd0c9b0e25455e6fe640808a
Elad Gubkin <Eladox3@gmail.com>
2021-11-22 23:50:06 +0200

    Deleted bar chat from apexcharts

--- src/components/charts/DayBars.jsx
+++ src/components/charts/DayBars.jsx
@@ -1,53 +1,8 @@
 import React, { useContext, useState } from "react";
 import { ChartsContext } from "../../contexts/ChartsContext";
-import Chart from "react-apexcharts";
 
 export const DayBars = () => {
   const { dayBarsData } = useContext(ChartsContext);
-
-  const [options] = useState({
-    plotOptions: {
-      bar: {
-        horizontal: true,
-        columnWidth: "55%",
-        endingShape: "rounded",
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      show: true,
-      width: 2,
-      colors: ["transparent"],
-    },
-    xaxis: {
-      categories: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    },
-    yaxis: {
-      title: {
-        text: "Number of Videos",
-      },
-    },
-    fill: {
-      opacity: 1,
-    },
-    tooltip: {
-      y: {
-        formatter: (val) => val,
-      },
-      X: {
-        formatter: (val) => val + "Day",
-      },
-    },
-  });
-
-  const [series] = useState([
-    {
-      name: "Videos Watched",
-      data: dayBarsData,
-    },
-  ]);
 
   return (
     <>
@@ -57,7 +12,6 @@ export const DayBars = () => {
       >
         dayBarsData
       </button>
-      <Chart options={options} series={series} type="bar" width={400} />;
     </>
   );
 };
