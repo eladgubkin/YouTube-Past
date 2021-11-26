637c98190a4c6c0e9c8cb9c3a10dbc5413532510
Elad Gubkin <Eladox3@gmail.com>
2021-11-26 22:52:15 +0200

    Working heatmap

--- src/components/charts/new/VidsPerDayHeatmap.jsx
+++ src/components/charts/new/VidsPerDayHeatmap.jsx
@@ -1,51 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { ChartsContext } from "../../../contexts/ChartsContext";
+import { FilesContext } from "../../../contexts/FilesContext";
+import { parseVidsPerDayHeatmap } from "../../../utils/charts/new/parseVidsPerDayHeatmap";
 import EChartsReact from "echarts-for-react";
 
 export const VidsPerDayHeatmap = () => {
-  const [data, setData] = useState([
-    ["2015-01-01", 99],
-    ["2015-01-02", 50],
-    ["2015-01-03", 25],
-    ["2015-01-04", 69],
-  ]);
+  const { watchHistoryData } = useContext(FilesContext);
+  const { vidsPerDayHeatmapData, setVidsPerDayHeatmapData } = useContext(ChartsContext);
+
+  useEffect(() => {
+    setVidsPerDayHeatmapData(parseVidsPerDayHeatmap(watchHistoryData));
+  }, []);
 
   const [option, setOption] = useState({
-    title: {
-      top: 30,
-      left: "center",
-      text: "Big Title Here",
-    },
     tooltip: {
       position: "top",
-      formatter: (p) => {
-        return "Lol";
-      },
+      formatter: (p) => `${p.data[1]} on ${p.data[0]}`,
     },
     visualMap: {
       min: 0,
       max: 100,
-      type: "piecewise",
-      orient: "horizontal",
-      left: "center",
-      top: 65,
+      calculable: true,
+      orient: "vertical",
+      top: "center",
     },
-    calendar: {
-      top: 120,
-      left: 30,
-      right: 30,
-      cellSize: ["auto", 13],
-      range: "2015",
-      itemStyle: {
-        borderWidth: 0.5,
+    calendar: [
+      {
+        orient: "horizontal",
+        range: "2021",
       },
-      yearLabel: { show: false },
-    },
-    series: {
-      type: "heatmap",
-      coordinateSystem: "calendar",
-      data: data,
-    },
+    ],
+    series: [
+      {
+        type: "heatmap",
+        coordinateSystem: "calendar",
+        calendarIndex: 0,
+        data: vidsPerDayHeatmapData,
+      },
+    ],
   });
 
   return <EChartsReact option={option} />;
+  // return <button onClick={() => console.log(vidsPerDayHeatmapData)}>Test</button>;
 };
