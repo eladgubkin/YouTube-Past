e876850d064c7cc7bafa345ad2f93a261b758f10
Elad Gubkin <Eladox3@gmail.com>
2021-11-30 23:02:02 +0200

    Heatmap

--- src/views/AppScreen.jsx
+++ src/views/AppScreen.jsx
@@ -3,20 +3,14 @@ import { Navbar } from "../Components/ui/Navbar";
 import { ChartsContextProvider } from "../contexts/ChartsContext";
 
 // Charts
-import { WordCloud } from "../components/charts/WordCloud";
-import { DayBars } from "../components/charts/DayBars";
-import { VideoBubbles } from "../components/charts/VideoBubbles";
-import { ChannelBubbles } from "../components/charts/ChannelBubbles";
-import { WorldMap } from "../components/charts/WorldMap";
-import { AvgVidsPerDayBars } from "../components/charts/new/AvgVidsPerDayBars";
-import { VidsPerDayHeatmap } from "../components/charts/new/VidsPerDayHeatmap";
+import { Heatmap } from "../components/charts/Heatmap";
 
 export const AppScreen = () => {
   return (
     <>
       <Navbar />
       <ChartsContextProvider>
-        <VidsPerDayHeatmap />
+        <Heatmap />
       </ChartsContextProvider>
     </>
   );
