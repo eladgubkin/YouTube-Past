4320a184a3caa70a14a48d00816aab1c434c0c29
Elad Gubkin <Eladox3@gmail.com>
2021-11-30 23:01:05 +0200

    clean files

--- src/utils/charts/videoBubbles/parseVideoBubblesData.js
+++
@@ -1,7 +0,0 @@
-import _ from "lodash";
-
-export const parseVideoBubblesData = (watchHistoryData) => {
-  let data = [];
-
-  return data;
-};
--- src/utils/charts/worldMap/parseWorldMapData.js
+++
@@ -1,7 +0,0 @@
-import _ from "lodash";
-
-export const parseWorldMapData = (watchHistoryData, locationHistoryData) => {
-  let data = [];
-
-  return data;
-};
