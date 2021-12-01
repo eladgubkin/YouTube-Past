f4daaa1f5b620f31527f8b429846931b2917122d
Elad Gubkin <Eladox3@gmail.com>
2021-12-01 23:43:19 +0200

    Updated imports

--- src/views/AppScreen.jsx
+++ src/views/AppScreen.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Navbar } from "../Components/ui/Navbar";
 import { ChartsContextProvider } from "../contexts/ChartsContext";
-
-// Charts
 import { Heatmap } from "../components/charts/Heatmap";
 
 export const AppScreen = () => {
