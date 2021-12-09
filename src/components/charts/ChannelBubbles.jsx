7ac89dbc3c213085637d6e60eb2650775bdfcd0b
Elad Gubkin <Eladox3@gmail.com>
2021-12-09 23:38:06 +0200

    Removed context

--- src/components/charts/ChannelBubbles.jsx
+++ src/components/charts/ChannelBubbles.jsx
@@ -1,16 +1,13 @@
-import React, { useContext, useEffect, useRef } from "react";
-import { ChartsContext } from "../../contexts/ChartsContext";
+import React, { useEffect, useRef } from "react";
 import { select, forceSimulation, forceX, forceY, forceCollide, scaleSqrt } from "d3";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-export const ChannelBubbles = () => {
-  const { channelBubblesData } = useContext(ChartsContext);
+export const ChannelBubbles = ({ data, updateData }) => {
   const svgRef = useRef();
 
-  // Run on mount
-  useEffect(() => {
+  const apply = () => {
     // Simulation
     const simulation = forceSimulation()
       .force("x", forceX().strength(0.05))
@@ -35,7 +32,7 @@ export const ChannelBubbles = () => {
 
     defs
       .selectAll(".channel-image")
-      .data(channelBubblesData)
+      .data(data)
       .enter()
       .append("pattern")
       .attr("class", "channel-image")
@@ -47,15 +44,12 @@ export const ChannelBubbles = () => {
       .attr("width", 1)
       .attr("height", 1)
       .attr("preserveAspectRatio", "none")
-      .attr("xlink:href", (d) => {
-        console.log(d);
-        return "https://yt3.ggpht.com/ytc/AKedOLS0jDvPKbVd8n0k9Kl76VTR5lUS4g-fAWyMuQgE_w=s176-c-k-c0x00ffffff-no-rj";
-      });
+      .attr("xlink:href", (d) => d.thumbnail);
 
     // Circles
     const circles = svg
       .selectAll(".channel")
-      .data(channelBubblesData)
+      .data(data)
       .enter()
       .append("circle")
       .attr("class", "channel")
@@ -64,18 +58,12 @@ export const ChannelBubbles = () => {
       .on("click", (d) => console.log(d.target.__data__.title));
 
     const ticked = () => circles.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-    simulation.nodes(channelBubblesData).on("tick", ticked);
-  }, []);
+    simulation.nodes(data).on("tick", ticked);
+  };
 
   return (
     <>
-      <button
-        className="bg-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium m-5"
-        onClick={() => console.log(channelBubblesData)}
-      >
-        ChannelBubble
-      </button>
-
+      <button onClick={apply}>Channel Bubbles</button>
       <svg ref={svgRef} />
     </>
   );
