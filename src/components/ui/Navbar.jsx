7bc2465f2314cf93cd0d5d22e60e9f993a4176c2
Elad Gubkin <Eladox3@gmail.com>
2021-12-10 23:52:26 +0200

    Removed Import button from nav

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -93,14 +93,6 @@ export const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <a
-              href="/import"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Import
-            </a>
-          </div>
         </div>
       </div>
 
