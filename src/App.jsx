a944bb20b76f85acbf303cef5cb62cac6e5fd7a6
Elad Gubkin <Eladox3@gmail.com>
2022-01-31 08:19:04 +0200

    formatOnSave

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"; // React
 import { useRecoilValue } from "recoil"; // Recoil
 import { watchHistoryAtom, searchHistoryAtom } from "./store/atoms"; // Atoms
-
 import { UploadPage } from "./pages/UploadPage"; // Component Page
 import { DashboardPage } from "./pages/DashboardPage"; // Component Page
 
