bd7ad4c5aa0172148d3ba99c4a7997a02d69ce68
Elad Gubkin <Eladox3@gmail.com>
2022-01-31 08:19:20 +0200

    Updated TailwindCSS

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from "react"; // React
 import ReactDOM from "react-dom"; // React
 import { RecoilRoot } from "recoil"; // Recoil
 import { App } from "./App"; // Component
-
 import { QueryClient, QueryClientProvider } from "react-query"; // React Query
+import "tailwindcss/tailwind.css"; // TailwindCSS
 
 const queryClient = new QueryClient();
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif", "Helvetica", "Arial"],
+      },
+    },
   },
   variants: {},
   plugins: [],
